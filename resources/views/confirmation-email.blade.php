fbfdf294c61d2abab405b5002e04d820f315d8db
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-21 12:19:58 +0400

    chore: blade for reset password

--- resources/views/confirmation-email.blade.php
+++ resources/views/confirmation-email.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="flex flex-col items-center h-screen lg:pb-8 ">
-        <img src="{{ asset('Landing.svg') }}" alt="dashboard-worldwide-screen" class="mt-4 " >
+        <img src="{{ asset('images/Landing.svg') }}" alt="dashboard-worldwide-screen" class="mt-4 " >
         <p class="text-lg lg:text-xl text-dark lg:mt-[60px] mt-[43px] mb-[8px]">{{ __('confirmation_email') }}</p>
         <p class="text-light-dark text-sm-1 lg:text-sm-4 mb-[24px]">{{ __('verify_email_btn') }}</p>
         <button type="submit" class="btn-primary md:w-[392px]">{{ __('verify_email') }}</button>
