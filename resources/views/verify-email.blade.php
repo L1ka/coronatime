990827164dd01a63f02749dc324ce4b24c3faf3a
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-23 15:58:37 +0400

    chore: lang file changes

--- resources/views/verify-email.blade.php
+++ resources/views/verify-email.blade.php
@@ -3,7 +3,7 @@
 
     <div id="container"  style="display: flex; flex-direction: column; align-items: center; height: 100vh;" >
         <img id="img" src="{{ asset('images/Landing.png') }}" alt="dashboard-worldwide-screen" style=" margin-top: 16px; width: 300px; height: 241px">
-        <p  style=" font-size: 20px; line-height: 24px; font-weight: bold; font-family: Inter; ">Confirmation email</p>
+        <p  style=" font-size: 20px; line-height: 24px; font-weight: bold; font-family: Inter; ">{{ __('confirm_email') }}</p>
         <p  style=" color: #808189; font-size: 16px; line-height: 19px; font-weight: 400; margin-bottom: 14px; font-family: Inter;">{{ __('verify_email_btn') }}</p>
         <a id="btn" href="{{ route('user.verify-email', ['token' => $user->verifyUser->token]) }}"
         style="line-height: 16px; font-size: 14px;  width: 300px; padding-top: 14px; padding-bottom: 14px; font-family: Inter;  text-decoration: none; color: #fff; background-color: #0FBA68; font-weight: 900; border-radius: 8px; text-align: center;">{{ __('verify_email') }}</a>
