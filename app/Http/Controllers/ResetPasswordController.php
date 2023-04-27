4def35e35f3222b5617473c74d93e397ed3df236
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-27 19:13:08 +0400

    refactor: use form request

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Password\ResetPasswordRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use App\Models\User;
@@ -35,13 +36,9 @@ class ResetPasswordController extends Controller
 
 
 
-    public function update(Request $request): RedirectResponse
+    public function update( ResetPasswordRequest $request): RedirectResponse
     {
-        $request->validate([
-            'token' => ['required' ],
-            'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed','min:3'],
-        ]);
+
 
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
