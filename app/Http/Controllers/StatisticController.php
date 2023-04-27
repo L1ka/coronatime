63c257c47279ea9c28562308db3a0487919aace1
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-27 13:18:51 +0400

    fix: sorting after refresh

---
+++ app/Http/Controllers/StatisticController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Statistic;
+use Illuminate\Contracts\View\View;
+
+
+class StatisticController extends Controller
+{
+
+    public function countries(): View
+    {
+        if (request('search')) {
+           $data =  Statistic::where('name', 'like', '%' . ucfirst(request('search')) . '%')->get();
+         }
+        elseif(request('sort')){
+          return $this->sort();
+        }
+        else{
+            $data = Statistic::all();
+        }
+
+        return view('dashboard-country', ['data' => $data, 'sum' => $this->countrySum()]);
+    }
+
+
+    public function sort(): View
+    {
+
+        if(request('sort') == 'asc' ) $data = Statistic::all()->sortBy(request('name'), SORT_REGULAR, true);
+
+        if(request('sort') == 'desc' ) $data = Statistic::all()->sortBy(request('name'), SORT_REGULAR, false);
+
+
+        return view('dashboard-country', ['data' => $data,  'sum' => $this->countrySum()]);
+    }
+
+    public function worldwide(): View
+    {
+        return view('dashboard-worldwide', [ 'sum' => $this->countrySum() ]);
+    }
+
+    public function countrySum()
+    {
+        $countrySum = [
+			'recover' => Statistic::sum('recover'),
+			'death'    => Statistic::sum('death'),
+			'new_case' =>  Statistic::sum('new_case'),
+		];
+        return  $countrySum;
+
+    }
+
+
+}
